ae2a356a7120a6ac8d491b24a4e613278d97511f
maruhsilva <marua.desenvolvedor@gmail.com>
2024-06-13 16:11:35 -0300

    teste

---
+++ scroll.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.nav', {duration: 3500});
+sr.reveal('.imagem', {duration: 3500});
+sr.reveal('.descricao', {duration: 3500});
+sr.reveal('.precos', {duration: 2500});
