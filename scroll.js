82277136cc960ae53f793dfffaab76434c6914da
maruhsilva <marua.desenvolvedor@gmail.com>
2024-06-17 14:51:46 -0300

    final responsivo

--- scroll.js
+++ scroll.js
@@ -3,4 +3,7 @@ window.sr = ScrollReveal({reset: true});
 sr.reveal('.nav', {duration: 3500});
 sr.reveal('.imagem', {duration: 3500});
 sr.reveal('.descricao', {duration: 3500});
-sr.reveal('.precos', {duration: 2500});
+sr.reveal('.servicos', {duration: 2500});
+sr.reveal('.precos', { rotate: { x: 0, y:5000, z:0}, duration: 2500});
+sr.reveal('.contato', {duration: 2000});
+sr.reveal('footer', {duration: 1000});
