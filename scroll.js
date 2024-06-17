b32ee19a3d40d7da3d1fc122c14cb5a058549970
maruhsilva <marua.desenvolvedor@gmail.com>
2024-06-17 16:26:01 -0300

    responsividade ok

--- scroll.js
+++ scroll.js
@@ -2,6 +2,7 @@ window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.imagem', {duration: 3500});
 sr.reveal('.fa-solid', {duration: 3500});
+sr.reveal('.fa-brands', {duration: 3500});
 sr.reveal('.descricao', {duration: 3500});
 sr.reveal('.servicos', {duration: 2500});
 sr.reveal('.precos', { rotate: { x: 0, y:5000, z:0}, duration: 2500});
