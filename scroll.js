f96d69091655d01f59017d4801a27baa71b7710c
maruhsilva <marua.desenvolvedor@gmail.com>
2024-06-17 16:10:56 -0300

    final responsividade

--- scroll.js
+++ scroll.js
@@ -1,9 +1,8 @@
 window.sr = ScrollReveal({reset: true});
 
-sr.reveal('.nav', {duration: 3500});
 sr.reveal('.imagem', {duration: 3500});
+sr.reveal('.fa-solid', {duration: 3500});
 sr.reveal('.descricao', {duration: 3500});
 sr.reveal('.servicos', {duration: 2500});
 sr.reveal('.precos', { rotate: { x: 0, y:5000, z:0}, duration: 2500});
 sr.reveal('.contato', {duration: 2000});
-sr.reveal('footer', {duration: 1000});
